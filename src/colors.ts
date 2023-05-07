2cb5a37a77ff5ab136cc417860aa79a007afe7f2
Garrett <gflynn@usc.edu>
2023-05-07 11:06:50 -0700

    Add plot with custom colors

---
+++ src/colors.ts
@@ -0,0 +1,22 @@
+
+export const V0 = 'hsl(322, 80%, 60%)'
+
+export const V1 = 'hsl(322, 0%, 94%)'
+export const V2 = 'hsl(322, 0%, 94%)'
+export const V3 = 'hsl(60, 80%, 60%)'
+export const V4 = 'hsl(60, 80%, 60%)'
+export const V5 = 'hsl(237, 80%, 60%)'
+export const V6 = 'hsl(237, 80%, 60%)'
+export const V7 = 'hsl(114, 80%, 60%)'
+export const V8 = 'hsl(114, 80%, 60%)'
+export const V9 = 'hsl(0, 80%, 60%)'
+export const V10 = 'hsl(17, 80%, 60%)'
+
+// Black
+export const V11 = 'hsl(0, 0%, 40%)'
+export const V12 = 'hsl(0, 0%, 35%)'
+export const V13 = 'hsl(0, 0%, 30%)'
+export const V14 = 'hsl(0, 0%, 25%)'
+export const V15 = 'hsl(0, 0%, 20%)'
+export const V16 = 'hsl(0, 0%, 15%)'
+export const V17 = 'hsl(0, 0%, 10%)'
--- src/pages/history.ts
+++ src/pages/history.ts
@@ -2,6 +2,10 @@ import { CommonElement } from '../../external/commonwealth/index.js'
 import listItem from '../components/li.js'
 import { get } from '../storage.js'
 
+import * as colors from '../colors'
+
+import 'https://cdn.plot.ly/plotly-2.20.0.min.js' // Use plotly
+
 export class HistoryPage extends CommonElement {
 
 
@@ -9,11 +13,65 @@ export class HistoryPage extends CommonElement {
         super(info)
     }
 
-    list() {
-        const dates = Array.from({ length: localStorage.length }, (_, i) => localStorage.key( i ))
+    entries() {
+        return Array.from({ length: localStorage.length }, (_, i) => localStorage.key( i ))
         .filter(str => str?.split('-').length === 3)
         .sort((o1,o2) => new Date(o2) - new Date(o1)) as string[] // Sort dates chronologically
+    }
 
+
+    plot() {
+        const dates = this.entries()
+        const entries = dates.map(date => get(date))
+
+        let grades: string[] = []
+        let gradeSplit = entries.map((arr, i) => {
+            
+            const acc: any = {}
+            arr.forEach(o => {
+                if (!grades.includes(o.value)) grades.push(o.value)
+                if (!(o.value in acc)) acc[o.value] = 1
+                else acc[o.value] ++
+            })
+
+            return acc
+        }, {})
+
+        const data = grades.sort((a,b) => {
+            return parseInt(a.slice(1)) - parseInt(b.slice(1))
+        }).map((v) => {
+            return {
+                x: dates,
+                y: gradeSplit.map(o => o[v] || 0),
+                name: v,
+                marker: {
+                    color: colors[v]
+                },
+                type: 'bar'
+              }
+        })
+
+
+          var layout = { 
+            barmode: 'stack', 
+            title: 'Climb History',
+            xaxis: {
+                type: 'category',
+                title: 'Date',
+            },
+            yaxis: {
+                title: '# of Climbs'
+            }
+        };
+
+          const div = document.createElement('div')
+          
+          setTimeout(() => Plotly.newPlot(div, data, layout), 10);
+          return div
+    }
+
+    list() {
+        const dates = this.entries()
         const container = document.createElement('div')
 
         container.append(...dates.map(date => {
@@ -48,6 +106,7 @@ export class HistoryPage extends CommonElement {
     render() {
         
         return [
+            this.plot,
             this.list,
         ]
         
