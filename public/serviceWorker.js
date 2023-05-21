d63495e4c70d7d24b19380ee45ad816cfd48b735
Garrett <gflynn@usc.edu>
2023-05-20 23:07:50 -0700

    Works locally

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,10 +1,13 @@
 const cacheName = "dev-justclimb";
 
+let pathname = globalThis.location.pathname
+if (pathname.includes('.html')) pathname = pathname.split('/').slice(0, -1).join('/')
+
 const assets = [
-  "/",
-  "/assets", // NOTE: This has to be solved for Vite
-  "/images/justclimb-logo-192x192.png",
-  "/images/justclimb-logo"
+  // pathname ?? '/', // NOTE: This breaks
+  `${pathname}/assets`, // NOTE: This has to be solved for Vite
+  `${pathname}/images/justclimb-logo-192x192.png`,
+  `${pathname}/images/justclimb-logo`
 ];
 
 self.addEventListener("install", installEvent => {
